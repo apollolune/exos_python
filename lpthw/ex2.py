b04cc82401bfbe1663629a941d23848a134e602f
apol <apollolune@gmail.com>
2012-03-04 14:36:16 +0100

    lpthw/: ajout exos

---
+++ lpthw/ex2.py
@@ -0,0 +1,10 @@
+# a comment
+# anything after the # is python
+
+print "I could have code like this" # and the comment after is ignored
+
+# you can also  use a "comment" to disable a line
+
+# print "This won't run"
+
+print "This will run"
